643e9c72ad3c79c1ca693c4596838e09214ed2a6
Umair <umairzahiraj16@gmail.com>
2025-05-04 20:54:28 +0530

    serverlet mapping complete

---
+++ serverlets/src/main/java/com/umair/serverlet/AddNumbers.java
@@ -0,0 +1,13 @@
+package com.umair.serverlet;
+
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class AddNumbers extends HttpServlet {
+
+}
---
+++ serverlets/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Add Numbers</h2>
+</body>
+</html>
