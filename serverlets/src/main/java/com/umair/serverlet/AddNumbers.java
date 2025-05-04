40e9b669084319b765a0745650f0903878d6bb90
Umair <umairzahiraj16@gmail.com>
2025-05-04 20:56:09 +0530

    Add route working fine basic servlet working fine

--- serverlets/src/main/java/com/umair/serverlet/AddNumbers.java
+++ serverlets/src/main/java/com/umair/serverlet/AddNumbers.java
@@ -9,5 +9,11 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class AddNumbers extends HttpServlet {
-
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+//        String[] values = request.getParameterValues("fieldName");
+        Writer wr = res.getWriter();
+        wr.write("Total : "+(num1+num2));
+    }
 }
